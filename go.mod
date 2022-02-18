e1512addb35ea1316ab0cb4a450f99e450a705c5
saitenntaisei <saitenntaisei@gmail.com>
2022-02-18 16:25:54 +0900

    firststep

--- go.mod
+++ go.mod
@@ -9,13 +9,13 @@ require (
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -22,7 +23,29 @@ func main() {
 		api.GET("/ping", func(c echo.Context) error {
 			return c.String(http.StatusOK, "pong")
 		})
+		api.GET("/ws", connectWS)
 	}
 
 	e.Logger.Panic(e.Start(":8080"))
+}
+
+var upgrader = websocket.Upgrader{}
+
+func connectWS(c echo.Context) error {
+	connection, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+	defer connection.Close()
+	for {
+		mesType, msg, err := connection.ReadMessage()
+		if err != nil {
+			return c.NoContent(http.StatusOK)
+		}
+		log.Printf("mesType= %d,msg=%s,", mesType, msg)
+		err = connection.WriteMessage(websocket.TextMessage, []byte("Hello"))
+		if err != nil {
+			return c.NoContent((http.StatusOK))
+		}
+	}
 }
